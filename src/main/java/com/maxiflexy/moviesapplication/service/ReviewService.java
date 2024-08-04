0eff8f55f1eb5933a848794d07f9d2639a14ccd7
Maxiflexy <onyemax247@gmail.com>
2024-08-04 23:35:40 +0100

    created review service

---
+++ src/main/java/com/maxiflexy/moviesapplication/service/ReviewService.java
@@ -0,0 +1,29 @@
+package com.maxiflexy.moviesapplication.service;
+
+import com.maxiflexy.moviesapplication.entity.Movie;
+import com.maxiflexy.moviesapplication.entity.Review;
+import com.maxiflexy.moviesapplication.repository.ReviewRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewService {
+
+    private final ReviewRepository reviewRepository;
+    private final MongoTemplate mongoTemplate;
+
+    public Review createReview(String reviewBody, String imdbId){
+        Review review = reviewRepository.insert(new Review(reviewBody));
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+
+        return review;
+    }
+}
