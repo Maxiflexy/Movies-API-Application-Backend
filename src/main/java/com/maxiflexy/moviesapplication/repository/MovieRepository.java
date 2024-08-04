2625b387702f45b3faf89d4c47b24c8b323b3147
Maxiflexy <onyemax247@gmail.com>
2024-08-04 22:06:34 +0100

    movie repository

---
+++ src/main/java/com/maxiflexy/moviesapplication/repository/MovieRepository.java
@@ -0,0 +1,10 @@
+package com.maxiflexy.moviesapplication.repository;
+
+import com.maxiflexy.moviesapplication.entity.Movie;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+}
