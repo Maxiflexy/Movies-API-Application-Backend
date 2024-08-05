21725c598821c98fbd33aa8302f28b8303ddcef7
Maxiflexy <onyemax247@gmail.com>
2024-08-05 13:02:30 +0100

    created review and movie controller

--- src/main/java/com/maxiflexy/moviesapplication/controller/MovieController.java
+++ src/main/java/com/maxiflexy/moviesapplication/controller/MovieController.java
@@ -6,16 +6,14 @@ import lombok.RequiredArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/v1/movies")
 public class MovieController {
 
--- src/main/java/com/maxiflexy/moviesapplication/controller/ReviewController.java
+++ src/main/java/com/maxiflexy/moviesapplication/controller/ReviewController.java
@@ -5,16 +5,14 @@ import com.maxiflexy.moviesapplication.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/reviews")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ReviewController {
 
     private final ReviewService reviewService;
